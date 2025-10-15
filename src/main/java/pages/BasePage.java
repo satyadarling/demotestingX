b07d64697587696273bde3dff8de424d32696b41
Satya Allumolu <your-email@example.com>
2025-10-15 11:18:28 +0530

    this is second comiting by x employ

--- src/main/java/pages/BasePage.java
+++ src/main/java/pages/BasePage.java
@@ -16,7 +16,7 @@ public class BasePage {
 
     public BasePage(WebDriver driver) {
         this.driver = driver;
-        this.wait=new WebDriverWait(driver,Duration.ofSeconds(20));
+        this.wait=new WebDriverWait(driver,Duration.ofSeconds(10));
     }
     
 
