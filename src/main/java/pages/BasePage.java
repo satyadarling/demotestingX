7d16260fa72d48ca5d999ce028a662cf3a56c6cc
Satya Allumolu <your-email@example.com>
2025-10-15 11:29:38 +0530

    this is third comiting by y employ

--- src/main/java/pages/BasePage.java
+++ src/main/java/pages/BasePage.java
@@ -16,7 +16,7 @@ public class BasePage {
 
     public BasePage(WebDriver driver) {
         this.driver = driver;
-        this.wait=new WebDriverWait(driver,Duration.ofSeconds(10));
+        this.wait=new WebDriverWait(driver,Duration.ofSeconds(25));
     }
     
 
